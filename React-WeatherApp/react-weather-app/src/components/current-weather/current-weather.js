0cd9351f6663017f4e3cf1b9c97b664e70e78bb7
Charles Neukuckatz <charneuk@gmail.com>
2023-01-26 15:34:04 -0600

    Adding Components and CSS

--- React-WeatherApp/react-weather-app/src/components/current-weather/current-weather.js
+++ React-WeatherApp/react-weather-app/src/components/current-weather/current-weather.js
@@ -4,12 +4,39 @@ const CurrentWeather = () => {
     return (
         <div className = "weather">
             <div className = "top">
+                <div>
                 <p className = "city">Belgrade</p>
                 <p className = "weather-description">Sunny</p>
             </div>
-            <img alt="weather" className ="weather-icon" src="public/01d.png" />
+            <img alt="weather" className ="weather-icon" src="01d.png" />
+        </div>
+    
+        <div className = "bottom">
+            <p className="temperature">18C</p>
+            <div classname = "details">
+                <div className = "parameter-row">
+                    <span className="parameter-label">Details</span>
+                </div>
+                <div className = "parameter-row">
+                    <span className="parameter-label">Feels like </span>
+                    <span className="parameter-value">22C </span>
+                </div>
+                <div className = "parameter-row">
+                    <span className="parameter-label">Wind </span>
+                    <span className="parameter-value"> 2 m/s </span>
+                </div>
+                <div className = "parameter-row">
+                    <span className="parameter-label">Humidity </span>
+                    <span className="parameter-value"> 15% </span>
+                </div>
+                <div className = "parameter-row">
+                    <span className="parameter-label">Pressure</span>
+                    <span className="parameter-value"> 15 hPa </span>
+                </div>
+        </div>
+        </div>
         </div>
     );
-}
+};
 
 export default CurrentWeather;
