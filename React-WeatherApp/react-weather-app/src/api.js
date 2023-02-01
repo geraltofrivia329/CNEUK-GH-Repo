b68ac29775b04fd2a5d7028c43f0b061142b718c
Charles Neukuckatz <charneuk@gmail.com>
2023-01-31 22:07:36 -0600

    WeatherFetchPromise

--- React-WeatherApp/react-weather-app/src/api.js
+++ React-WeatherApp/react-weather-app/src/api.js
@@ -1,10 +1,12 @@
 export const geoApioptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Key': 'dc7acb7cd5msh637f5f5bacf3190p114f48jsndc7133db34ae',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions"
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d9732ec013eb2f1c05f9a422721c7623";
 
