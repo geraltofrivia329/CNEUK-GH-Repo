8991c5cc51b9edd1d0d78a678eec5122df3bf44d
Charles Neukuckatz <charneuk@gmail.com>
2023-02-01 21:59:56 -0600

    RefiningAPIkeys

--- React-WeatherApp/react-weather-app/src/api.js
+++ React-WeatherApp/react-weather-app/src/api.js
@@ -6,7 +6,7 @@ export const geoApioptions = {
 	}
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = "d9732ec013eb2f1c05f9a422721c7623";
 
